6240d41786230b826db328bc6309cd564d5e08aa
Farhana Ahmed Fima <farhanaahmedfima@Farhanas-MBP.box>
2023-10-15 08:53:37 +0200

    show and hide informations

--- lib/description/ui/widget/owner_info.dart
+++ lib/description/ui/widget/owner_info.dart
@@ -33,63 +33,69 @@ class _OwnerInfoState extends State<OwnerInfo> {
               onPressed: () {
                 setState(() {
                   selected = !selected;
-                  print(selected);
+                  //print(selected);
                 });
               },
               icon: selected
                   ? const Icon(
-                      Icons.arrow_upward,
+                      Icons.keyboard_arrow_up,
                       size: 32,
                     )
                   : const Icon(
-                      Icons.arrow_downward,
+                      Icons.keyboard_arrow_down,
                       size: 32,
                     ),
             ),
           ],
         ),
-        Row(
-          children: [
-            Image.network(
-              "${widget.item.owner!.avatarUrl}",
-              height: 60,
-              width: 60,
-            ),
-            const SizedBox(
-              width: 10,
-            ),
-            Text(
-              "${widget.item.owner!.login}",
-              style: const TextStyle(
-                fontSize: 20,
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            OwnerUrls(
-                urlText: "Repositories", url: widget.item.owner!.reposUrl!),
-            const SizedBox(
-              height: 10,
-            ),
-            OwnerUrls(urlText: "Gists", url: widget.item.owner!.gistsUrl!),
-            const SizedBox(
-              height: 10,
-            ),
-            OwnerUrls(
-                urlText: "Followers", url: widget.item.owner!.followersUrl!),
-            const SizedBox(
-              height: 10,
-            ),
-            OwnerUrls(
-                urlText: "Followings", url: widget.item.owner!.followingUrl!),
-          ],
-        ),
+       if(selected)
+         Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: [
+             Row(
+               children: [
+                 Image.network(
+                   "${widget.item.owner!.avatarUrl}",
+                   height: 60,
+                   width: 60,
+                 ),
+                 const SizedBox(
+                   width: 10,
+                 ),
+                 Text(
+                   "${widget.item.owner!.login}",
+                   style: const TextStyle(
+                     fontSize: 20,
+                   ),
+                 ),
+               ],
+             ),
+             const SizedBox(
+               height: 20,
+             ),
+             Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 OwnerUrls(
+                     urlText: "Repositories", url: widget.item.owner!.reposUrl!),
+                 const SizedBox(
+                   height: 10,
+                 ),
+                 OwnerUrls(urlText: "Gists", url: widget.item.owner!.gistsUrl!),
+                 const SizedBox(
+                   height: 10,
+                 ),
+                 OwnerUrls(
+                     urlText: "Followers", url: widget.item.owner!.followersUrl!),
+                 const SizedBox(
+                   height: 10,
+                 ),
+                 OwnerUrls(
+                     urlText: "Followings", url: widget.item.owner!.followingUrl!),
+               ],
+             ),
+           ],
+         ),
       ],
     );
   }
